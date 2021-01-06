ddfe8ef08b684d78d9129948a0370f6409749586
Tyler Samuelson <tylercsamuelson@gmail.com>
2021-01-06 10:49:27 -0700

    Tried removing hard-coded PORT var in hopes of fixing Heroku deploy error

--- server/server.js
+++ server/server.js
@@ -1,37 +1,37 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const { basename } = require('path');
-const fs = require('fs');
+const { basename } = require("path");
+const fs = require("fs");
 
-const app = require('express')();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const next = require('next');
-const compression = require('compression');
-const accepts = require('accepts');
+const app = require("express")();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const next = require("next");
+const compression = require("compression");
+const accepts = require("accepts");
 
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== "production";
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-const auth = require('./auth');
-const api = require('./api');
-const glob = require('glob');
+const auth = require("./auth");
+const api = require("./api");
+const glob = require("glob");
 
 // Get the supported languages by looking for translations in the `lang/` dir.
-const languages = glob.sync('./lang/*.json').map(f => basename(f, '.json'));
+const languages = glob.sync("./lang/*.json").map(f => basename(f, ".json"));
 
 // We need to expose React Intl's locale data on the request for the user's
 // locale. This function will also cache the scripts by lang in memory.
 const localeDataCache = new Map();
 const getLocaleDataScript = locale => {
-  const lang = locale.split('-')[0];
+  const lang = locale.split("-")[0];
   if (!localeDataCache.has(lang)) {
     const localeDataFile = require.resolve(`react-intl/locale-data/${lang}`);
-    const localeDataScript = fs.readFileSync(localeDataFile, 'utf8');
+    const localeDataScript = fs.readFileSync(localeDataFile, "utf8");
     localeDataCache.set(lang, localeDataScript);
   }
   return localeDataCache.get(lang);
@@ -47,8 +47,8 @@ const getMessages = locale => {
 nextApp.prepare().then(() => {
   app.use(compression());
   app.use(cookieParser());
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
+  app.set("views", __dirname + "/views");
+  app.set("view engine", "ejs");
 
   // needed to parse refresh_token
   app.use(
@@ -58,11 +58,11 @@ nextApp.prepare().then(() => {
   );
 
   // auth router
-  app.use('/auth', auth);
+  app.use("/auth", auth);
 
-  app.use('/api', api(io));
+  app.use("/api", api(io));
 
-  app.get('*', (req, res) => {
+  app.get("*", (req, res) => {
     const accept = accepts(req);
     const locale = accept.language(/*dev ? ['en'] : */ languages);
     req.locale = locale;
